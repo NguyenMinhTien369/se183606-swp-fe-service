f048e3240ec6870d178c27c55e46f547b4d705bd
NguyenMinhTien369 <162936465+NguyenMinhTien369@users.noreply.github.com>
2025-11-15 23:24:49 +0700

    Chưa có code mới tạo component

---
+++ src/pages/SC_Staff/ManageCustomers/components/NotRegisteredAlert.tsx
@@ -0,0 +1,37 @@
+// import React from "react";
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+// } from "@/components/ui/dialog";
+// import { Button } from "@/components/ui/button";
+
+// export default function NotRegisteredAlert() {
+//   return (
+//     <div>
+//       <Dialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
+//         <DialogContent>
+//           <DialogHeader>
+//             <DialogTitle>
+//               Không tìm thấy khách hàng với từ khóa tìm kiếm
+//             </DialogTitle>
+//             <DialogDescription>
+//               Bạn có muốn thêm khách hàng mới không ?
+//             </DialogDescription>
+//           </DialogHeader>
+//           <DialogFooter className="gap-2">
+//             <Button variant="outline" onClick={handleCancelDelete}>
+//               Hủy
+//             </Button>
+//             <Button variant="destructive" onClick={handleConfirmDelete}>
+//               Xóa
+//             </Button>
+//           </DialogFooter>
+//         </DialogContent>
+//       </Dialog>
+//     </div>
+//   );
+// }
